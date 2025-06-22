fcbeb5ee4b8bd74746a50b4423292a318dae310f
xunchang <1460538972@qq.com>
2025-06-22 21:52:14 +0800

    配置Redis代码

---
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -0,0 +1,32 @@
+package com.sky.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+*@BelongsProject: sky-take-out
+*@BelongsPackage: com.sky.config
+*@Author: tan
+*@CreateTime: 2025-06-21  22:47
+*@Description: Redis配置类
+*/
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        log.info("开始创建redis模板对象……");
+        RedisTemplate redisTemplate = new RedisTemplate();
+        //设置Redis的连接工厂对象
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //设置Redis key的序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+}
